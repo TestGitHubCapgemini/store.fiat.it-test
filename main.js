61426368807f93c1558942439bab14a4deb9565e
CabraGrande <125824683+CabraGrande@users.noreply.github.com>
2023-02-19 20:01:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 function myFunction() {
             
-    let nameVal = document.getElementById("fname").value;
-    let lastNameVal = document.getElementById("lname").value;
-    alert("The form was submitted " + nameVal + " " + lastNameVal);
-  }
-
-  if (window.addEventListener) {
-    // For standards-compliant web browsers
-    window.addEventListener("message", handleMessage, false);
-  } else {
-    window.attachEvent("onmessage", handleMessage);
-  }
-
-  function handleMessage(evt){
-    
+      var message = document.getElementById('vfMessage').value;
+            window.parent.postMessage(message, lexOrigin);
   }
