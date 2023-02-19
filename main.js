5a5938b2d057ee859a54817122f1ef976a8c7804
CabraGrande <125824683+CabraGrande@users.noreply.github.com>
2023-02-19 21:19:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 function myFunction() {
-    let lexOrigin = "";
     
-          window.addEventListener("message", function(event) {
-            console.log('Origin '+ event.origin);
-            console.log('data '+ event.data);
-            lexOrigin =  event.data;
-
-        });
-    
+let lexOrigin = "https://scacorp--scadev3.sandbox.lightning.force.com/lightning/action/quick/Lead.POC_Tarificar?objectApiName&context=RECORD_DETAIL&recordId=00Q1x000006zvGhEAI&backgroundContext=%2Flightning%2Fr%2FLead%2F00Q1x000006zvGhEAI%2Fview";
     let nameVal = document.getElementById("fname").value;
     let lastNameVal = document.getElementById("lname").value;
     let message = "Este es el nombre " +nameVal + " " +  lastNameVal;        
