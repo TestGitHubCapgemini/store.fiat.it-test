2302378ef732561464494a3f72a6c3493282d9af
CabraGrande <125824683+CabraGrande@users.noreply.github.com>
2023-02-21 23:28:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let lexOrigin;  
-window.addEventListener("message", handleMessage(window.event), false);
+window.addEventListener("message", function(event) {
+            console.log('Origin '+ event.origin);
+            console.log('data '+ event.data);
+       console.log('Origin '+ evt.origin);
+            console.log('data '+ evt.data);
+            lexOrigin =  evt.data;
 
+        });
 
 function myFunction() {
 //let lexOrigin = "https://scacorp--scadev3.sandbox.lightning.force.com/lightning/action/quick/Lead.POC_Tarificar?objectApiName&context=RECORD_DETAIL&recordId=00Q1x000006zvGhEAI&backgroundContext=%2Flightning%2Fr%2FLead%2F00Q1x000006zvGhEAI%2Fview";
