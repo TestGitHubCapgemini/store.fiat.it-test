62a454a4c56e1b2130f8ed215ba0879948a7f5fb
CabraGrande <125824683+CabraGrande@users.noreply.github.com>
2023-02-22 00:04:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-let lexOrigin;  
-window.addEventListener("message", handleMessage , false);
+
 
 function myFunction() {
 //let lexOrigin = "https://scacorp--scadev3.sandbox.lightning.force.com/lightning/action/quick/Lead.POC_Tarificar?objectApiName&context=RECORD_DETAIL&recordId=00Q1x000006zvGhEAI&backgroundContext=%2Flightning%2Fr%2FLead%2F00Q1x000006zvGhEAI%2Fview";
 
     let nameVal = document.getElementById("fname").value;
     let lastNameVal = document.getElementById("lname").value;
-    let message = "Este es el nombre " +nameVal + " " +  lastNameVal;        
+    let message = "Este es el nombre " +nameVal + " " +  lastNameVal;
+    
+    let lexOrigin;  
+    window.addEventListener("message", handleMessage() , false);
+    
     alert("The form was submitted " + nameVal + " " + lastNameVal);
             window.parent.postMessage(message, lexOrigin);
   }
