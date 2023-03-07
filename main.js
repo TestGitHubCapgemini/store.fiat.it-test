bd0b6dc0d72cdd4cd0716aac883deabff6dc5ee0
CabraGrande <125824683+CabraGrande@users.noreply.github.com>
2023-03-07 11:12:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
 
 
 function myFunction() {
-//let lexOrigin = "https://scacorp--scadev3.sandbox.lightning.force.com/lightning/action/quick/Lead.POC_Tarificar?objectApiName&context=RECORD_DETAIL&recordId=00Q1x000006zvGhEAI&backgroundContext=%2Flightning%2Fr%2FLead%2F00Q1x000006zvGhEAI%2Fview";
-    console.log("aglo");
+    let lexOrigin = "https://scacorp--scadev3.sandbox.lightning.force.com/lightning/action/quick/Lead.POC_Tarificar?objectApiName&context=RECORD_DETAIL&recordId=00Q1x000006zvGhEAI&backgroundContext=%2Flightning%2Fr%2FLead%2F00Q1x000006zvGhEAI%2Fview";
+    console.log("Entra en myFunction");
     let nameVal = document.getElementById("fname").value;
     let lastNameVal = document.getElementById("lname").value;
-    let message = "Este es el nombre " +nameVal + " " +  lastNameVal;
+    let message = "Este es el nombre " +nameVal + " " +  lastNameVal; 
     
-    let lexOrigin = "https://github.com/CabraGrande/FormularioTest/edit/main/main.js";  
-   
-    
-    alert("The form was submitted " + nameVal + " " + lastNameVal);
+    alert("The form was submitted " +message);
             window.parent.postMessage(message, lexOrigin);
   }
-
-  /*if (window.addEventListener) {
-    // For standards-compliant web browsers
-    window.addEventListener("message", handleMessage, false);
-  } else {
-    window.attachEvent("onmessage", handleMessage);
-  }*/
 
 function createEvent(){
     console.log("Entra en onload del frame");
